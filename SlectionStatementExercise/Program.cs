794ced3e64c60c8f3d97c1fd0b11d4ddccb0d20e
Harold Bengter <Haroldbe55@yahoo.com>
2022-07-07 12:21:41 -0400

    Finished the exercise

---
+++ SlectionStatementExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿//*Exercise one*\\
+    var r = new Random();
+    var myNumber = r.Next(1, 500);
+
+    int userInput;
+
+    Console.Write("Pick a number between 1 and 500.");
+
+
+
+
+    do
+    {
+        userInput = int.Parse(Console.ReadLine());
+
+        if (myNumber < userInput)
+        {
+            Console.WriteLine("That's a good guess but too high! Try again! ");
+        }
+        else if (myNumber > userInput)
+        {
+            Console.WriteLine("That's a little low. Try Again! ");
+        }
+        else
+        {
+            Console.WriteLine("That's the one! Great job! ");
+        }
+
+        Console.Write("Take another guess: ");
+    }
+
+    while (userInput != myNumber);
